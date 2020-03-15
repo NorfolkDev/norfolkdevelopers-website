466730445499b4a15f61ab7ceba58aeb1a4b49bd
Shaun Church <shaun@shaunchurch.com>
2020-03-15 21:34:33 +0000

    Adjust post template

--- src/components/layout/PostTemplate.tsx
+++ src/components/layout/PostTemplate.tsx
@@ -3,13 +3,16 @@ import Layout from "./Layout";
 export default function PostTemplate({ frontMatter, children }) {
   return (
     <Layout>
-      <article className="mt-20">
+      <article className="mt-20 lg:max-w-3xl mr-auto ml-auto">
+        {frontMatter.date && (
+          <span className="inset inline-block text-gray-600">
+            {frontMatter.date}
+          </span>
+        )}
         <h1 className="inset text-5xl font-bold leading-tight mb-4">
           {frontMatter.title}
         </h1>
-        <span className="inset inline-block text-gray-600">
-          {frontMatter.date}
-        </span>
+
         <main className="typography inset mt-8">{children}</main>
       </article>
     </Layout>
