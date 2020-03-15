f5e0d44b0b3cbadb3c2667e11e9f39ebda71e819
Shaun Church <shaun@shaunchurch.com>
2020-03-15 15:56:58 +0000

    Use correct directory for purgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,10 @@ const purgecss = [
   "@fullhuman/postcss-purgecss",
   {
     content: [
-      "./components/**/*.js",
-      "./pages/**/*.js",
-      "./components/**/*.tsx",
-      "./pages/**/*.tsx"
+      "./src/components/**/*.js",
+      "./src/pages/**/*.js",
+      "./src/components/**/*.tsx",
+      "./src/pages/**/*.tsx"
     ],
     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
   }
