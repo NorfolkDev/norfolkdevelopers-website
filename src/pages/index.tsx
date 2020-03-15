b7cf4116fc236b74c00833136ab82c8ad043c221
Shaun Church <shaun@shaunchurch.com>
2020-03-15 21:35:09 +0000

    Change homepage styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,37 @@ import Layout from "../components/layout/Layout";
 export default function IndexRoute() {
   return (
     <Layout>
-      <main className="inset typography mt-20 text-2xl">
-        <p>Hello, I'm Shaun 👋</p>
+      <main className="inset typography mt-20 text-5xl">
         <p>
-          I like{" "}
+          Hello, I'm <span className="font-bold">Shaun</span> 👋
+        </p>
+        <p>
+          I mostly{" "}
           <Link href="/apps">
-            <a>making things</a>
+            <a>
+              <span className="text-blue-600">make things</span>
+            </a>
           </Link>{" "}
-          with 🖤 and 💻. Sometimes I{" "}
+          with 🖤 and 💻.
+        </p>
+        <p className="text-2xl text-gray-500">
+          Sometimes I{" "}
           <Link href="/music">
-            <a>write songs</a>
+            <a>
+              <span className="text-red-600">write songs</span>
+            </a>
           </Link>{" "}
           and{" "}
           <Link href="/words">
-            <a>words</a>
+            <a>
+              <span className="text-green-600">words</span>
+            </a>
           </Link>{" "}
           as well.
+        </p>
+        <p className="text-2xl text-gray-500">
+          Come at me on twitter{" "}
+          <a href="https://twitter.com/shaunchurch">@shaunchurch</a>.
         </p>
       </main>
     </Layout>
