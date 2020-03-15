bab3cfcda6fbe508e32a540419cd491c4c338bf4
Shaun Church <shaun@shaunchurch.com>
2020-03-15 19:19:52 +0000

    Adjust menu items

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,15 +2,16 @@ import Link from "next/link";
 import config from "../../../site.config";
 
 const navLinks = [
-  { url: `/${config.postsDirectory}`, label: "blog" },
-  { url: "/about", label: "about" }
+  { url: `/${config.postsDirectory}`, label: "words" },
+  { url: "/apps", label: "apps" },
+  { url: "/music", label: "music" }
 ];
 
 export default function Layout({ children }) {
   return (
     <div className="ml-auto mr-auto w-full lg:w-2/3">
       <header>
-        <h1 className="text-center pt-16 pb-2 font-bold text-3xl hover:text-blue-400">
+        <h1 className="text-center pt-16 pb-2 font-bold text-3xl hover:text-blue-600">
           <Link href="/">
             <a>{config.siteName}</a>
           </Link>
@@ -18,7 +19,7 @@ export default function Layout({ children }) {
         <nav className="text-2xl text-center font-normal">
           {navLinks.map(navLink => (
             <Link href={navLink.url} key={navLink.label}>
-              <a className="pr-2 pl-2 text-blue-400 hover:text-blue-700">
+              <a className="pr-2 pl-2 text-blue-600 hover:text-blue-400">
                 {navLink.label}
               </a>
             </Link>
