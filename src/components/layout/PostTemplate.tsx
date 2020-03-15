1ca605396b4f093df3d0029ebdeb7218d7855ae1
Shaun Church <shaun@shaunchurch.com>
2020-03-15 18:58:09 +0000

    Redesign post template to be more spacious

--- src/components/layout/PostTemplate.tsx
+++ src/components/layout/PostTemplate.tsx
@@ -3,10 +3,14 @@ import Layout from "./Layout";
 export default function PostTemplate({ frontMatter, children }) {
   return (
     <Layout>
-      <article className="p-2">
-        <h1 className="text-5xl font-bold">{frontMatter.title}</h1>
-        <span>{frontMatter.date}</span>
-        <main>{children}</main>
+      <article className="mt-20">
+        <h1 className="inset text-5xl font-bold leading-tight mb-4">
+          {frontMatter.title}
+        </h1>
+        <span className="inset inline-block text-gray-600">
+          {frontMatter.date}
+        </span>
+        <main className="typography inset mt-8">{children}</main>
       </article>
     </Layout>
   );
