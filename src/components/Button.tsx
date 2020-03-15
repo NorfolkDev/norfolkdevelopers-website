ee3f2f12c57539daaffafc933f3d7e3c9a5a1916
Shaun Church <shaun@shaunchurch.com>
2020-03-15 18:58:38 +0000

    Add basic Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <button className="bg-red-500 hover:bg-red-700 rounded-md shadow-sm pt-1 pb-1 pl-4 pr-4 text-white">
+      Button
+    </button>
+  );
+}
