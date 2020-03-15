d2d5e343ecdd7a7f4701a64a5d56fd08617b678d
Shaun Church <shaun@shaunchurch.com>
2020-03-15 13:06:02 +0000

    Add basic page Layout component

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+const navLinks = [
+  { url: "/words", label: "words" },
+  { url: "/music", label: "music" },
+  { url: "/apps", label: "apps" },
+  { url: "/about", label: "about" }
+];
+
+export default function Layout({ children }) {
+  return (
+    <div className="ml-auto mr-auto w-full lg:w-2/3">
+      <header>
+        <h1 className="text-center pt-16 pb-2 font-bold text-3xl">
+          <Link href="/">
+            <a>shaun.church</a>
+          </Link>
+        </h1>
+        <nav className="text-2xl text-center font-normal">
+          {navLinks.map(navLink => (
+            <Link href={navLink.url} key={navLink.label}>
+              <a className="pr-2 pl-2 text-blue-700">{navLink.label}</a>
+            </Link>
+          ))}
+        </nav>
+      </header>
+      {children}
+    </div>
+  );
+}
