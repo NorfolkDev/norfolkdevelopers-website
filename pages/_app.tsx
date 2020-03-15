9a8705d6e9ee680043c6d78ddee42a7e59e57554
Shaun Church <shaun@shaunchurch.com>
2020-03-15 12:57:42 +0000

    Organise files

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { AppProps } from "next/app";
-import "../css/tailwind.css";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
