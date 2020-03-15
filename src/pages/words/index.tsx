e8ed7a2f5d495954d214e5594155b502f1463d61
Shaun Church <shaun@shaunchurch.com>
2020-03-15 20:04:37 +0000

    Adjust /words styles

--- src/pages/words/index.tsx
+++ src/pages/words/index.tsx
@@ -2,16 +2,17 @@ import Link from "next/link";
 import Layout from "../../components/layout/Layout";
 
 function WordsRoute({ posts }) {
+  // console.log(posts);
   return (
     <Layout>
-      <h1 className="inset mt-8 text-4xl font-extrabold ">/words [index]</h1>
+      <h1 className="inset mt-8 text-3xl font-extrabold pb-4">/words</h1>
       <main className="inset mt-4">
         <ul>
           {posts.map(post => (
             <li key={post.path} className="mb-4">
               <span className="block text-base text-gray-500">{post.date}</span>
               <Link href={post.path || "/"}>
-                <a className="text-blue-700 hover:text-blue-400">
+                <a className="text-2xl font-semibold text-blue-700 hover:text-blue-400">
                   {post.title}
                 </a>
               </Link>
