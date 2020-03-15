b5d8dde9886202fabe2d347d2691504e4e6d0b46
Shaun Church <shaun@shaunchurch.com>
2020-03-15 19:20:11 +0000

    Add links to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
+import Link from "next/link";
 import Layout from "../components/layout/Layout";
 
 export default function IndexRoute() {
   return (
     <Layout>
-      <p className="text-green-500">Hello world!</p>
+      <main className="inset typography mt-20 text-2xl font-light">
+        <p>Hello, I'm Shaun 👋</p>
+        <p>
+          I like{" "}
+          <Link href="/apps">
+            <a>making things</a>
+          </Link>{" "}
+          with 🖤 and 💻. Sometimes I{" "}
+          <Link href="/music">
+            <a>write songs</a>
+          </Link>{" "}
+          and{" "}
+          <Link href="/words">
+            <a>words</a>
+          </Link>{" "}
+          as well.
+        </p>
+      </main>
     </Layout>
   );
 }
