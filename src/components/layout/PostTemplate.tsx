3876986e320085d77adb1b9195bf06a8a40e2667
Shaun Church <shaun@shaunchurch.com>
2020-03-16 13:43:57 +0000

    Add post page title and switch frontMatter to post

--- src/components/layout/PostTemplate.tsx
+++ src/components/layout/PostTemplate.tsx
@@ -2,25 +2,29 @@ import Layout from "./Layout";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { slugify } from "../../lib/slugify";
+import Head from "next/head";
 
-export default function PostTemplate({ frontMatter, children }) {
+export default function PostTemplate({ frontMatter: post, children }) {
   const router = useRouter();
 
   return (
     <Layout>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
       <article className="article mt-8 lg:max-w-3xl mr-auto ml-auto">
-        {frontMatter.date && (
+        {post.date && (
           <pre className="inset inline-block text-gray-500 text-sm">
-            {frontMatter.date}
+            {post.date}
           </pre>
         )}
         <pre className="inset block text-gray-500 text-sm">{router.asPath}</pre>
         <h1 className="inset hashtag mt-2 mb-2 text-5xl font-bold leading-tight">
-          {frontMatter.title}
+          {post.title}
         </h1>
-        {frontMatter.tags ? (
+        {post.tags ? (
           <pre className="inset">
-            {frontMatter.tags.map(tag => (
+            {post.tags.map(tag => (
               <span
                 className=" rounded-full bg-gray-200 p-1 mr-2 text-xs font-bold text-gray-600"
                 key={tag}
@@ -32,15 +36,15 @@ export default function PostTemplate({ frontMatter, children }) {
             ))}
           </pre>
         ) : null}
-        {frontMatter.author ? (
+        {post.author ? (
           <span className="inset block text-base mt-4 text-gray-600">
             by{[" "]}
-            {frontMatter.author.map((author, i) => (
+            {post.author.map((author, i) => (
               <span key={author}>
                 <Link href={`/author/${slugify(author)}`}>
                   <a className="underline">{author}</a>
                 </Link>
-                {i < frontMatter.author.length - 1 ? ", " : ""}
+                {i < post.author.length - 1 ? ", " : ""}
               </span>
             ))}
           </span>
