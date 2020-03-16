a23b7a8ecba312e6cdd5205f58eff54f845cd3a6
Shaun Church <shaun@shaunchurch.com>
2020-03-16 09:51:58 +0000

    Use posts cache and fetch tag posts

--- src/lib/get-posts.ts
+++ src/lib/get-posts.ts
@@ -9,6 +9,8 @@ const POSTS_DIRECTORY = path.join(
   `/src/pages/${POSTS_DIRECTORY_NAME}`
 );
 
+let cache = { posts: [] };
+
 function isFile(path: string): boolean {
   return path.includes(".");
 }
@@ -23,7 +25,8 @@ function getPostPaths(dir: string = "/") {
         !file.endsWith(".tsx") &&
         !file.endsWith(".ts") &&
         !file.endsWith(".js") &&
-        !file.endsWith(".jsx")
+        !file.endsWith(".jsx") &&
+        !file.endsWith(".DS_Store")
     );
 
   return [
@@ -35,8 +38,15 @@ function getPostPaths(dir: string = "/") {
   ];
 }
 
+type FrontMatter = {
+  body: string;
+  path: string;
+  tags?: string[];
+  author?: string[];
+};
+
 // load file contents and front matter
-function getPostData(posts: string[]) {
+function getPostData(posts: string[]): FrontMatter[] {
   return posts.map(postPath => {
     const filename = path.parse(postPath);
     const file: string = fs.readFileSync(
@@ -55,17 +65,42 @@ function getPostData(posts: string[]) {
   });
 }
 
-export function getPosts() {
+export function getPosts(useCache?: boolean) {
   const TAG = "[ posts ]";
+  if (useCache && cache.posts !== []) {
+    console.log(`${TAG} Using cached posts...`);
+    return cache.posts;
+  }
+  cache.posts = [];
   console.log(`${TAG} scanning for posts`, `pages/${POSTS_DIRECTORY_NAME}/**`);
   const postPaths = getPostPaths();
   postPaths.forEach(path => console.log(`${TAG} -`, path));
-  return getPostData(postPaths).sort(
+  const posts = getPostData(postPaths).sort(
     (a, b) =>
       // TODO type front matter import
       // @ts-ignore
       new Date(b.date).getMilliseconds() - new Date(a.date).getMilliseconds()
   );
+  cache.posts = posts;
+  return posts;
 }
 
-export function getTags() {}
+export function getTags() {
+  return getPosts(true).reduce((acc, current, index) => {
+    console.log("index", index, acc);
+    if (!current.tags) {
+      return acc;
+    }
+    current.tags.forEach(tag => {
+      if (!acc[tag]) {
+        acc[tag] = [];
+      }
+      acc[tag].push(current);
+    });
+    return acc;
+  });
+}
+
+export function getPostsForTag(tag: string) {
+  return getTags()[tag];
+}
