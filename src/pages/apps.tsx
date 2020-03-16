409ec3eaca802a32ecca92581957046acbf3cbf5
Shaun Church <shaun@shaunchurch.com>
2020-03-16 08:02:58 +0000

    Add placeholder pages

---
+++ src/pages/apps.tsx
@@ -0,0 +1,5 @@
+import Layout from "../components/layout/Layout";
+
+export default function AppsRoute() {
+  return <Layout location="apps"> Apps</Layout>;
+}
---
+++ src/pages/music.tsx
@@ -0,0 +1,5 @@
+import Layout from "../components/layout/Layout";
+
+export default function MusicRoute() {
+  return <Layout location="music">Music</Layout>;
+}
