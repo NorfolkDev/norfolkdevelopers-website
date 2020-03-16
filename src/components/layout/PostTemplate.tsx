65322d630f8533387ae5cf4d8142be1f6452cf62
Shaun Church <shaun@shaunchurch.com>
2020-03-16 08:02:11 +0000

    Redesign post tempalte

--- src/components/layout/PostTemplate.tsx
+++ src/components/layout/PostTemplate.tsx
@@ -1,18 +1,37 @@
 import Layout from "./Layout";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function PostTemplate({ frontMatter, children }) {
+  const router = useRouter();
+
+  console.log(frontMatter.tags);
   return (
     <Layout>
-      <article className="mt-20 lg:max-w-3xl mr-auto ml-auto">
+      <article className="mt-8 lg:max-w-3xl mr-auto ml-auto">
         {frontMatter.date && (
-          <span className="inset inline-block text-gray-600">
+          <pre className="inset inline-block text-gray-600 text-sm">
             {frontMatter.date}
-          </span>
+          </pre>
         )}
-        <h1 className="inset text-5xl font-bold leading-tight mb-4">
+        <pre className="inset block text-gray-600 text-sm">{router.asPath}</pre>
+        <h1 className="inset hashtag mt-2 mb-2 text-5xl font-bold leading-none">
           {frontMatter.title}
         </h1>
-
+        {frontMatter.tags ? (
+          <pre className="inset">
+            {frontMatter.tags
+              .split(",")
+              .map(tag => tag.trim())
+              .map(tag => (
+                <span className=" rounded-full bg-gray-200 p-1 mr-2 text-xs font-bold text-gray-600">
+                  <Link href={`/tag/${tag}`}>
+                    <a> #{tag} </a>
+                  </Link>
+                </span>
+              ))}
+          </pre>
+        ) : null}
         <main className="typography inset mt-8">{children}</main>
       </article>
     </Layout>
