168e71827135c2122f9944c5beb2752ec1ddbe27
Shaun Church <shaun@shaunchurch.com>
2020-04-04 16:39:16 +0100

    Add code syntax highlighting in MDX files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const rehypePrism = require("@mapbox/rehype-prism");
 
 const nextConfig = {
   pageExtensions: ["js", "jsx", "tsx", "mdx", "md"],
@@ -7,13 +8,18 @@ const nextConfig = {
       test: /\.(md|mdx)$/,
       use: [
         options.defaultLoaders.babel,
-        "@mdx-js/loader",
-        path.join(__dirname, "./src/lib/frontmatter-loader")
-      ]
+        {
+          loader: "@mdx-js/loader",
+          options: {
+            rehypePlugins: [rehypePrism],
+          },
+        },
+        path.join(__dirname, "./src/lib/frontmatter-loader"),
+      ],
     });
 
     return config;
-  }
+  },
 };
 
 module.exports = nextConfig;
