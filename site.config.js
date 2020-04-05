b1301fd890d94bd9ce7f896945506c26994bde8f
Shaun Church <shaun@shaunchurch.com>
2020-04-05 22:50:08 +0100

    Add numLatestPosts

--- site.config.js
+++ site.config.js
@@ -1,10 +1,13 @@
 const siteConfig = {
-  siteName: "blog.com",
+  siteName: "://newblog.com",
   description: "This is a blog about things.",
   postsDirectory: "words",
   features: {
     authorPages: false,
     tagPages: false,
+  },
+  settings: {
+    numLatestPosts: 3, // the number of posts to show in latest posts
   },
 };
 
