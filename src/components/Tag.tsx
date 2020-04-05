6dd91e924939385880d997112b8fc982c7c67fc3
Shaun Church <shaun@shaunchurch.com>
2020-04-05 12:33:18 +0100

    Example post layout

---
+++ src/components/Tag.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import siteConfig from "../../site.config";
+
+type Props = {
+  tag: string;
+};
+
+const TAG_COLOURS = {
+  music: {
+    background: "bg-red-200",
+    foreground: "text-red-800",
+  },
+  dev: {
+    background: "bg-green-200",
+    foreground: "text-green-800",
+  },
+  test: {
+    background: "bg-blue-200",
+    foreground: "text-blue-800",
+  },
+  default: {
+    background: "bg-grey-300",
+    foreground: "text-grey-700",
+  },
+};
+
+function getClasses(key: string) {
+  if (!(key in TAG_COLOURS)) {
+    key = "default";
+  }
+  return `${TAG_COLOURS[key].background} ${TAG_COLOURS[key].foreground}`;
+}
+
+export default function Tag(props: Props) {
+  const { tag } = props;
+
+  return (
+    <span
+      className={`rounded-full bg-gray-200 p-1 mr-2 text-xs font-bold text-gray-600 px-2 ${getClasses(
+        tag
+      )}`}
+      key={tag}
+    >
+      {siteConfig.features.tagPages ? (
+        <Link href={`/tag/${tag}`}>
+          <a> #{tag} </a>
+        </Link>
+      ) : (
+        <a> #{tag} </a>
+      )}
+    </span>
+  );
+}
--- src/components/layout/PostTemplate.tsx
+++ src/components/layout/PostTemplate.tsx
@@ -1,55 +1,50 @@
 import Layout from "./Layout";
-import { useRouter } from "next/router";
 import Link from "next/link";
-import { slugify } from "../../lib/slugify";
 import Head from "next/head";
+import siteConfig from "../../../site.config";
+import { slugify } from "../../lib/slugify";
+import { dateFormat } from "../../lib/date-functions";
+import Tag from "../Tag";
 
 export default function PostTemplate({ frontMatter: post, children }) {
-  const router = useRouter();
-
   return (
     <Layout>
       <Head>
         <title>{post.title}</title>
       </Head>
       <article className="article mt-8 lg:max-w-3xl mr-auto ml-auto">
-        {post.date && (
-          <pre className="inset inline-block text-gray-500 text-sm">
-            {post.date}
-          </pre>
-        )}
-        <pre className="inset block text-gray-500 text-sm">{router.asPath}</pre>
-        <h1 className="inset hashtag mt-2 mb-2 text-5xl font-bold leading-tight">
-          {post.title}
-        </h1>
-        {post.tags ? (
-          <pre className="inset">
-            {post.tags.map(tag => (
-              <span
-                className=" rounded-full bg-gray-200 p-1 mr-2 text-xs font-bold text-gray-600"
-                key={tag}
-              >
-                <Link href={`/tag/${tag}`}>
-                  <a> #{tag} </a>
-                </Link>
-              </span>
-            ))}
-          </pre>
-        ) : null}
-        {post.author ? (
-          <span className="inset block text-base mt-4 text-gray-600">
-            by{[" "]}
-            {post.author.map((author, i) => (
-              <span key={author}>
-                <Link href={`/author/${slugify(author)}`}>
-                  <a className="underline">{author}</a>
-                </Link>
-                {i < post.author.length - 1 ? ", " : ""}
-              </span>
-            ))}
-          </span>
-        ) : null}
-        <main className="typography inset mt-8">{children}</main>
+        <header className="inset mb-12">
+          {post.tags ? (
+            <div className="inline">
+              {post.tags.map((tag) => (
+                <Tag tag={tag} />
+              ))}
+            </div>
+          ) : null}
+
+          <h1 className="hashtag mt-2 mb-2 text-5xl font-bold leading-tight">
+            {post.title}
+          </h1>
+          {post.date && (
+            <p className="block text-gray-500 text-sm font-bold">
+              {dateFormat(new Date(post.date))}
+            </p>
+          )}
+          {post.author && siteConfig.features.authorPages ? (
+            <span className="block text-base text-gray-600">
+              by{[" "]}
+              {post.author.map((author, i) => (
+                <span key={author}>
+                  <Link href={`/author/${slugify(author)}`}>
+                    <a className="underline">{author}</a>
+                  </Link>
+                  {i < post.author.length - 1 ? ", " : ""}
+                </span>
+              ))}
+            </span>
+          ) : null}
+        </header>
+        <div className="typography inset">{children}</div>
       </article>
     </Layout>
   );
