10cfdff4337069040cbdc5fe62a86dd22a146ce0
Shaun Church <shaun@shaunchurch.com>
2020-06-10 12:13:55 +0100

    Remove experimental redirect

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const rehypePrism = require("@mapbox/rehype-prism");
 
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx", "md"],
-  experimental: {
-    async rewrites() {
-      return [
-        {
-          source: "/rss",
-          destination: "/api/rss",
-        },
-      ];
-    },
-  },
 
   webpack(config, options) {
     config.module.rules.push({
