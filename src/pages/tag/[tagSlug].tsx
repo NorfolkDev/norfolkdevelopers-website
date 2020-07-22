030157d4763d1b83708074ac365e432383dca8a4
Shaun Church <shaun@shaunchurch.com>
2020-07-22 12:05:05 +0100

    Add tag slug page template

--- src/pages/tag/[tagSlug].tsx
+++ src/pages/tag/[tagSlug].tsx
@@ -1,43 +1,56 @@
-import config from "../../../site.config";
+import siteConfig from "site.config";
 import { GetStaticProps } from "next";
-import { PostData, getTags } from "@static-fns/blog";
+import { PostData, getTags, getStaticTagPaths } from "@static-fns/blog";
+import Layout from "src/components/layout/Layout";
+import PostCard from "src/components/PostCard";
 
 export async function getStaticPaths() {
-  if (!config.features.tagPages) {
+  if (!siteConfig.features.tagPages) {
     return {
       paths: [],
       fallback: false,
     };
   }
-
-  const paths = Object.keys(getTags()).map((tag) => {
-    return { params: { tagSlug: tag } };
-  });
-
   return {
-    paths: paths,
+    paths: getStaticTagPaths(),
     fallback: false,
   };
 }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+type Props = {
+  posts: PostData[];
+  tagSlug: string;
+};
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  const tagSlug = params?.tagSlug as string;
+  const posts = getTags();
+  const tagPosts = posts[tagSlug];
+
   return {
     props: {
-      // @ts-ignore
-      posts: getTags()[params.tagSlug],
+      posts: tagPosts, // posts,
+      tagSlug,
     },
   };
 };
 
-type Props = {
-  posts: PostData[];
-};
-export default function TagSlug({ posts }: Props) {
+export default function TagSlug({ posts, tagSlug }: Props) {
   return (
-    <div>
-      {posts.map((post) => (
-        <div key={post.title}>{post.title}</div>
-      ))}
-    </div>
+    <Layout location="words" title={`Posts tagged #${tagSlug}`}>
+      <h1
+        className="mt-8 text-3xl font-bold pb-4 lg:max-w-3xl ck
+      mr-auto ml-auto"
+      >
+        /tag/{tagSlug}
+      </h1>
+      <main className="mt-4 border-gray-600 important:mr-auto important:ml-auto block">
+        <ul className="-mx-4">
+          {posts.map((post) => (
+            <PostCard key={post.path} post={post} />
+          ))}
+        </ul>
+      </main>
+    </Layout>
   );
 }
