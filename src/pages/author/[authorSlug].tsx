f760388c585306445494ac322e74f16a544fccaf
Shaun Church <shaun@shaunchurch.com>
2020-07-24 19:58:35 +0100

    Use local slugify

--- src/pages/author/[authorSlug].tsx
+++ src/pages/author/[authorSlug].tsx
@@ -1,11 +1,11 @@
 import { useCallback } from "react";
 import { GetStaticProps } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import { PostData, getAuthors, slugify } from "@static-fns/blog";
+import { PostData, getAuthors } from "@static-fns/blog";
 import Layout from "../../components/layout/Layout";
 import PostCard from "../../components/PostCard";
 import siteConfig from "../../../site.config";
+import { slugify } from "src/slugify";
 
 export async function getStaticPaths() {
   // no pagesif disables
@@ -61,7 +61,9 @@ export default function AuthorSlug({ posts, slug }: Props) {
           {siteConfig.siteName}/{title}
         </title>
       </Head>
-      <h1 className="mt-8 text-3xl font-bold pb-4 lg:max-w-3xl mr-auto ml-auto">{title}</h1>
+      <h1 className="mt-8 text-3xl font-bold pb-4 lg:max-w-3xl mr-auto ml-auto">
+        {title}
+      </h1>
       <main className="mt-4 border-gray-600 important:mr-auto important:ml-auto block">
         <ol className="-mx-4">
           {posts.map((post: PostData) => (
