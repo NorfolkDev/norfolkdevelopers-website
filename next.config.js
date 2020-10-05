227fb1f0e7d3359b892b7c0abb186c298c4d032e
Alex Scotton <alex@alexscotton.com>
2020-10-05 12:03:05 +0100

    WIP - Author deets

--- next.config.js
+++ next.config.js
@@ -22,6 +22,20 @@ const nextConfig = {
         {
           loader: "@static-fns/loader",
         },
+        createLoader(function (src) {
+          const content = [
+            'import Post from "@/components/Post"',
+            'export { getStaticProps } from "@/getStaticProps"',
+            src,
+            'export default (props) => <Post meta={meta} {...props} />',
+          ].join('\n')
+
+          if (content.includes('<!--​more-->')) {
+            return this.callback(null, content.split('<!--​more-->').join('\n'))
+          }
+
+          return this.callback(null, content.replace(/<!--​excerpt-->.*<!--\/excerpt-->/s, ''))
+        }),
       ],
     });
 
