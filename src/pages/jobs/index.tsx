6c91c8720e980ddefff0feb9654c22763b368cd1
Alex Scotton <alex@alexscotton.com>
2022-10-08 16:17:45 +0100

    Generate Jobs listing

--- src/pages/jobs/index.tsx
+++ src/pages/jobs/index.tsx
@@ -3,55 +3,63 @@ import Head from "next/head";
 import siteConfig from "site.config";
 import JobCard from "src/components/JobCard";
 import getConfig from "next/config";
-// const { serverRuntimeConfig } = getConfig();
-import { isAfter as isAfterDate } from "date-fns";
-
-export async function getStaticProps() {
-  return {
-    props: { jobs: [] },
-    // props: { jobs: JSON.parse(JSON.stringify(getPosts({ directory }))) },
-  };
-}
+import { allJobs, Job } from "contentlayer/generated";
+import { compareDesc } from "date-fns";
+import { GetStaticProps } from "next";
+import Pagination from "src/components/Pagination";
 
 type Props = {
-  jobs: [];
+  jobs: Job[];
+  page: number;
+  total: number;
 };
 
-export default function JobsRoute({ jobs }: Props) {
-  return (
-    <div>
-      {/* @TODO: Make not "broken" :S */}
-      <p>Broken</p>
-    </div>
-  );
-  // const activeJobs = jobs.filter(
-  //   (job) => job.expiryDate && isAfterDate(new Date(job.expiryDate), new Date())
-  // );
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  return {
+    props: {
+      page: 1,
+      // @TODO: Refactor this out, into a data provider
+      jobs: allJobs
+        .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+        .slice(0, siteConfig.settings.postsPerPage),
+      total: allJobs.length,
+    },
+  };
+};
 
-  // return (
-  //   <Layout location="jobs">
-  //     <Head>
-  //       <title>
-  //         {siteConfig.siteName}/{siteConfig.jobsDirectory}
-  //       </title>
-  //     </Head>
-  //     <section className="section" id="posts">
-  //       <h1 className="pb-4 mt-8 ml-auto mr-auto text-3xl font-bold lg:max-w-3xl">
-  //         <span className="font-bold text-red-500">/</span>
-  //         {siteConfig.jobsDirectory}
-  //       </h1>
-  //       <main className="block mt-4 border-gray-600 important:mr-auto important:ml-auto">
-  //         {activeJobs.length === 0 ? (
-  //           <h2>There doesn't seem to be anything here.</h2>
-  //         ) : (
-  //           <ul className="-mx-4">
-  //             {activeJobs.map((post) => (
-  //               <JobCard key={post.path} job={post} />
-  //             ))}
-  //           </ul>
-  //         )}
-  //       </main>
-  //     </section>
-  //   </Layout>
-  // );
+export default function JobsRoute({ jobs, page, total }: Props) {
+  return (
+    <Layout location="jobs">
+      <Head>
+        <title>
+          {siteConfig.siteName}/{siteConfig.jobsDirectory}
+        </title>
+      </Head>
+      <section className="section" id="posts">
+        <h1 className="pb-4 mt-8 ml-auto mr-auto text-3xl font-bold lg:max-w-3xl">
+          <span className="font-bold text-red-500">/</span>
+          {siteConfig.jobsDirectory}
+        </h1>
+        <main className="block mt-4 border-gray-600 important:mr-auto important:ml-auto">
+          {jobs.length === 0 ? (
+            <h2>There doesn't seem to be anything here.</h2>
+          ) : (
+            <ul className="-mx-4">
+              {jobs.map((job) => (
+                <JobCard key={job.url} job={job} />
+              ))}
+            </ul>
+          )}
+
+          <Pagination
+            root="/jobs"
+            seperator="p"
+            page={page}
+            total={total}
+            perPage={siteConfig.settings.postsPerPage}
+          />
+        </main>
+      </section>
+    </Layout>
+  );
 }
