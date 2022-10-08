cc32ff5c26326f50dbd65a1a78fd77a753d81d78
Alex Scotton <alex@alexscotton.com>
2022-10-08 10:35:42 +0100

    Update Next to v12 (functional but broken build)

--- next.config.js
+++ next.config.js
@@ -2,36 +2,10 @@ const path = require("path");
 const rehypePrism = require("@mapbox/rehype-prism");
 
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx", "md"],
+  pageExtensions: ["js", "jsx", "ts", "tsx", /* "mdx",  "md" */],
 
   serverRuntimeConfig: {
     PROJECT_ROOT: path.join(__dirname, 'src'),
-  },
-
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.(md|mdx)$/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: "@mdx-js/loader",
-          options: {
-            rehypePlugins: [rehypePrism],
-          },
-        },
-        {
-          loader: "@static-fns/loader",
-        },
-      ],
-    });
-
-    if (!options.isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
-
-    return config;
   },
 };
 
