b7fc3afa90cd546c6876757d8512a599c37ba6b0
Alex Scotton <alex@alexscotton.com>
2022-10-19 14:39:48 +0100

    Fix: Prettier

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,39 +1,40 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files';
-import removeMarkdown from 'remove-markdown';
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import removeMarkdown from "remove-markdown";
+import siteConfig from "./site.config";
 
 export const Post = defineDocumentType(() => ({
-  name: 'Post',
-  contentType: 'mdx',
+  name: "Post",
+  contentType: "mdx",
   filePathPattern: `posts/**/*.mdx`,
   fields: {
     title: {
-      type: 'string',
-      description: 'The title of the post',
+      type: "string",
+      description: "The title of the post",
       required: true,
     },
     date: {
-      type: 'date',
-      description: 'The date of the post',
+      type: "date",
+      description: "The date of the post",
       required: true,
     },
     author: {
-      type: 'string',
-      description: 'The author of the post',
+      type: "string",
+      description: "The author of the post",
       required: true,
     },
     tags: {
-      type: 'string',
+      type: "string",
     },
     hero: {
-      type: 'string'
+      type: "string",
     },
     layout: {
-      type: 'string',
+      type: "string",
     },
   },
   computedFields: {
     url: {
-      type: 'string',
+      type: "string",
       resolve: (post) => `/${post._raw.flattenedPath}`,
     },
     editUrl: {
@@ -42,90 +43,93 @@ export const Post = defineDocumentType(() => ({
         `${siteConfig.githubUrl}edit/master/content/${post._raw.sourceFilePath}`,
     },
     slug: {
-      type: 'string',
+      type: "string",
       // @TODO: Investigate removing the call to the replace method, is there a better contentlayer prop?
-      resolve: (post) => post._raw.flattenedPath.replace('posts/', ''),
+      resolve: (post) => post._raw.flattenedPath.replace("posts/", ""),
     },
     // @TODO: Support these natively in the markdown posts (with actual yaml lists)
     authors: {
-      type: 'array',
-      resolve: (post) => post.author.split(', '),
+      type: "array",
+      resolve: (post) => post.author.split(", "),
     },
     tagList: {
-      type: 'array',
-      resolve: (post) => post.tags.split(', '),
+      type: "array",
+      resolve: (post) => post.tags.split(", "),
     },
     excerpt: {
-      type: 'string',
-      resolve: (post) => removeMarkdown(post.body.raw).slice(0, 280) + '...',
-    }
+      type: "string",
+      resolve: (post) => removeMarkdown(post.body.raw).slice(0, 280) + "...",
+    },
   },
-}))
+}));
 
 export const Job = defineDocumentType(() => ({
-  name: 'Job',
-  contentType: 'mdx',
-  filePathPattern: 'jobs/**/*.mdx',
+  name: "Job",
+  contentType: "mdx",
+  filePathPattern: "jobs/**/*.mdx",
   fields: {
     title: {
-      type: 'string',
-      description: 'The title of the job',
+      type: "string",
+      description: "The title of the job",
       required: true,
     },
     date: {
-      type: 'date',
-      description: 'The date the job was posted',
+      type: "date",
+      description: "The date the job was posted",
       required: true,
     },
     expiryDate: {
-      type: 'date',
-      description: 'The date the job was posted',
+      type: "date",
+      description: "The date the job was posted",
       required: true,
     },
     role: {
-      type: 'string',
-      description: 'The job role',
+      type: "string",
+      description: "The job role",
       required: true,
     },
     salary: {
-      type: 'string',
-      description: 'The job salary',
+      type: "string",
+      description: "The job salary",
       required: true,
     },
     company: {
-      type: 'string',
-      description: 'The company that\'s recruiting for this role',
+      type: "string",
+      description: "The company that's recruiting for this role",
       required: true,
     },
     location: {
-      type: 'string',
-      description: 'The location required for this role (Remote is a valid option)',
+      type: "string",
+      description:
+        "The location required for this role (Remote is a valid option)",
       required: true,
     },
     seniority: {
-      type: 'string',
-      description: 'What seniority is this position (e.g. Senior, Junior etc)',
+      type: "string",
+      description: "What seniority is this position (e.g. Senior, Junior etc)",
       required: true,
     },
     apply: {
-      type: 'string',
-      description: 'How should applicants apply (Used as a link, mailto is a valid option)',
+      type: "string",
+      description:
+        "How should applicants apply (Used as a link, mailto is a valid option)",
       required: true,
     },
     layout: {
-      type: 'string',
-      description: 'How should applicants apply (Used as a link, mailto is a valid option)',
+      type: "string",
+      description:
+        "How should applicants apply (Used as a link, mailto is a valid option)",
     },
   },
   computedFields: {
     url: {
-      type: 'string',
+      type: "string",
       resolve: (post) => `/jobs/${post._raw.flattenedPath}`,
     },
   },
-}))
+}));
 
 export default makeSource({
-  contentDirPath: 'content',
+  contentDirPath: "content",
   documentTypes: [Post, Job],
-})
+});
