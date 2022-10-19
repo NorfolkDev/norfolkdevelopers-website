3dc1bc2307a703288ff77b90b7b40336aa792cb1
Alex Scotton <alex@alexscotton.com>
2022-10-19 16:22:29 +0100

    Fix: Remove hover border on PostCard

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export default function PostCard({ post }: Props) {
   return (
-    <li className="flex flex-col px-4 py-4 mb-6 rounded md:flex-row hover:border">
+    <li className="flex flex-col px-4 py-4 mb-6 rounded md:flex-row">
       <div className="w-full mb-1 mr-6 md:mt-2 md:mb-0 md:w-2/12">
         {post.tagList && <TagList tags={post.tagList} />}
       </div>
