81276981bdec436b28c48749df685fedd09ff11a
Alex Scotton <alex@alexscotton.com>
2022-10-19 14:39:39 +0100

    Refactor editUrl into contentLayer

--- src/pages/posts/[postSlug].tsx
+++ src/pages/posts/[postSlug].tsx
@@ -6,7 +6,6 @@ import Pagination from "../../components/Pagination";
 import { allPosts, Post } from "contentlayer/generated";
 import TagList from "src/components/TagList";
 import { Fragment } from "react";
-import { useRouter } from "next/router";
 import Link from "next/link";
 import { slugify } from "src/slugify";
 import PostDate from "src/components/PostDate";
@@ -56,9 +55,6 @@ export default function PostSlug({ post }: Props) {
   }
 
   const MDXContent = useMDXComponent(post.body.code);
-  const router = useRouter();
-  // @TODO: Refactor into a computed field
-  let editUrl = `${siteConfig.githubUrl}edit/master/src/pages/${router.pathname}/index.mdx`;
 
   return (
     <Layout>
@@ -108,7 +104,7 @@ export default function PostSlug({ post }: Props) {
         <footer className="py-4 mt-6 text-base">
           <a
             className="text-foreground-secondary hover:text-foreground-primary hover:underline"
-            href={editUrl}
+            href={post.editUrl}
             target="_blank"
             rel="nofollow"
           >
