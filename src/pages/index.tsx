36472e641761ab4e3a0d26d7efa24e6e4d85bc77
Alex Scotton <alex@alexscotton.com>
2022-12-04 19:53:38 +0000

    Reduce latest posts on home page to 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts: Posts.slice(0, siteConfig.settings.postsPerPage),
+      posts: Posts.slice(0, 3),
       initialEventData,
     },
     revalidate: 60,
